57993b1f3902b7aaf82e98ab843486977cc151d7
Salas-Maximo <maxi5salas@gmail.com>
2024-12-03 17:14:50 -0300

    subo el proyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import JuegoContador from "./Juegocontador";
+
+function App() {
+  return (
+    <div>
+      <JuegoContador />
+    </div>
+  );
+}
+
+export default App;
